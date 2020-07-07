9c1f594758bdfa79bd8d32e2cc8b85841414c81c
HuNtErJ1324 <peanutsareawesome@outlook.com>
2020-07-07 12:28:07 -0700

    updated

--- src/networking/Client.java
+++ src/networking/Client.java
@@ -5,10 +5,44 @@
  */
 package networking;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
  *
  * @author peanu
  */
 public class Client {
-    
+
+    public static void main(String[] args) {
+        int port = 8000;
+        String host = "localhost";
+        DataInputStream in;
+        DataOutputStream out;
+        Socket socket = null;
+        Scanner input = new Scanner(System.in);
+        String text = "";
+
+        try {
+            socket = new Socket(host, port);
+            System.out.println("Client connected to port " + port);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            while (!text.equals("end")) {
+                text = input.nextLine();
+                out.writeUTF(text);
+                out.flush();
+                System.out.println(in.readUTF());
+            }
+            socket.close();
+
+        } catch (IOException e) {
+            System.out.println("oopsies");
+        }
+    }
 }
--- src/networking/Server.java
+++ src/networking/Server.java
@@ -5,10 +5,47 @@
  */
 package networking;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 /**
  *
  * @author peanu
  */
 public class Server {
-    
+
+    public static void main(String[] args) {
+        int port = 8000;
+        DataInputStream in;
+        BufferedReader d;
+        DataOutputStream out;
+        ServerSocket server;
+        Socket socket;
+        Scanner input = new Scanner(System.in);
+        String text = "";
+
+        try {
+            server = new ServerSocket(port);
+            System.out.println("Server listening on port " + port);
+            socket = server.accept();
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
+            while (!text.equals("end")) {
+                System.out.println(in.readUTF());
+                text = input.nextLine();
+                out.writeUTF(text);
+                out.flush();
+            }
+            server.close();
+        } catch (IOException e) {
+            System.out.println("oopsies");
+        }
+
+    }
 }
